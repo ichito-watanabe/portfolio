44e2169f7bd06da5fea58677bdc13f384b7d7b0d
ichito-watanabe <s221089@stn.nagaokaut.ac.jp>
2026-03-23 01:27:09 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const introTitle = "Welcome to Ichito Portfolio";
 const introProfileLines = [
   "出身地 ： 諏訪市",
   "大学 ： 長岡技術科学大学",
-  "趣味 ： ダンス，麻雀",
+  "趣味 ： ダンス， 音楽を聴くこと",
 ] as const;
 const introLines = [introTitle, ...introProfileLines] as const;
 const introWidthReference = introTitle;
